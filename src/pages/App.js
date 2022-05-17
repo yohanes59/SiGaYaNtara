97bbc18e4eaf71d1e0493244dfb5c4eeccd974c1
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2022-05-18 02:01:37 +0700

    adding routing for Home

---
+++ src/pages/App.js
@@ -0,0 +1,9 @@
+import Routes from "../config/Routes";
+
+function App() {
+  return (
+    <Routes />
+  )
+}
+
+export default App;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="container">
+            <p className="text-warning">Test</p>
+        </div>
+    )
+}
+
+export default Home;
